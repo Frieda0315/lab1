36e1894753dbccfdd90fd3652da69824e730a331
Frieda0315 <59812863+Frieda0315@users.noreply.github.com>
2021-09-07 23:15:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import requests
 response = requests.get('http://google.com/')
 
 print('Your IP is {0}'.format(response.json()['origin']))
+
+pythonS = requests.get("https://raw.githubusercontent.com/Frieda0315/lab1/master/main.py")
+print(pythonS.text)
